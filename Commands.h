b44a4bc88b3fd7df0f92abded9da7f654cce56ca
Zaid Taiyab <ztaiyab@asu.edu>
2024-05-17 10:18:52 -0700

    Update Commands.h

--- Commands.h
+++ Commands.h
@@ -1,1 +1,20 @@
+#ifndef CPPMUD_COMMANDS_H
+#define CPPMUD_COMMANDS_H
+#include "World.h"
+#include "Player.h"
+#include <string>
+#include <iostream>
 
+using namespace std;
+
+class Commands {
+
+public:
+
+    bool doCommand(World *myWorld, Player *player, string input);
+
+    string getCommands();
+};
+
+
+#endif
