b385a786f37c005397dba7f84d6058e4dbf5057b
Zaid Taiyab <ztaiyab@asu.edu>
2024-05-22 18:48:30 -0700

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,1 +1,18 @@
+#ifndef CPPMUD_GAME_H
+#define CPPMUD_GAME_H
+#include "World.h"
+#include "Player.h"
 
+class Game{
+private:
+    World *myWorld;
+    Player *player;
+
+public:
+    Game(World *mw);
+    void start();
+
+
+};
+
+#endif //CPPMUD_GAME_H
